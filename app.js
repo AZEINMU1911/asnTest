f978e7a21c858297d3b94bf315a1df91844d0003
Mike <alfzein19@gmail.com>
2025-01-17 13:13:15 +0700

    Correct more spelling mistake and added pagination + filter WIP in app.js

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
-//start
+// start
 require('dotenv').config();
 const client = require('./db');
 const express = require('express');
-const db = require('./db');
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT || 3000; 
 
 app.use(express.json());
 
-//GET
-app.get('/api/countries', async (req,res) => {
-    try{
+// GET
+app.get('/api/countries', async (req, res) => {
+    try {
         const result = await client.query('SELECT * FROM countries');
-        res.json(result.rows)
-    } catch(err) {
-        console.error('Error querying database', err)
-        res.status(500).json({error : 'Database Error'});
-    };
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error querying database', err);
+        res.status(500).json({ error: 'Database Error' });
+    }
 });
 
-app.listen(port , () => {
-    console.log (`Server running on http://localhost:${port}`)
+//GET Pagination and filter
+app.get('/api/countries', async (req,res) => {
+    const { page = 1, limit = 10, name, capital, code} = req.querry;
+    const offset = (page - 1) * limit;
+
+    let query = 'SELECT * FROM countries WHERE true';
+    const queryParams = [];
+
+    if (name){
+        query += 'AND name ILIKE $' + (queryParams.length + 1);
+        queryParams.push(`%${name}`);
+    }
+    if (capital){
+        query += 'AND name ILIKE $' + (queryParams.length + 1);
+        queryParams.push(`%${name}`);
+    }
+})
+
+// Test route to check database connection
+app.get('/api/test', async (req, res) => {
+    try {
+        const result = await client.query('SELECT NOW()');
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error connecting to database', err);
+        res.status(500).json({ error: 'Database connection error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
--- db.js
+++ db.js
@@ -1,13 +1,14 @@
-require ('dontenv').config();
-const {Clients} = require('pg');
+require('dotenv').config();
+const { Client } = require('pg');
 
-const client = new Client ({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    pasword: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 5432,
+const client = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,
 });
 
 client.connect();
+
 module.exports = client;
